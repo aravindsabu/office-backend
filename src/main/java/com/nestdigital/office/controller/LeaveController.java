67e94a93f87e79891b0ace55cb2581995ce0cb53
aravindsabu <aravindsabu2001@gmail.com>
2022-10-28 16:57:12 +0530

    ok

--- src/main/java/com/nestdigital/office/controller/LeaveController.java
+++ src/main/java/com/nestdigital/office/controller/LeaveController.java
@@ -1,4 +1,51 @@
 package com.nestdigital.office.controller;
+import com.nestdigital.office.dao.LeaveDao;
+import com.nestdigital.office.model.LeaveModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import javax.transaction.Transactional;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Map;
 
 public class LeaveController {
+    @Autowired
+    private LeaveDao leaveDao;
+    //Add leave applications in employee
+    @CrossOrigin(origins = "*")
+    @PostMapping(path = "/applyLeave",consumes = "application/json",produces = "application/json")
+    public String applyLeave(@RequestBody LeaveModel model){
+        DateTimeFormatter date = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        model.setApplyDate((String.valueOf(date.format(now))));
+        model.setStatus(0);
+        leaveDao.save(model);
+        return "Success";
+    }
+
+    //change leave status in admin
+    @Transactional
+    @CrossOrigin("*")
+    @PostMapping("/leaveStatus")
+    public String changeLeaveStatus(@RequestBody LeaveModel model){
+        leaveDao.changeStatusOfLeave(model.getId(), model.getStatus());
+        return "Success'";
+    }
+
+    //View All leaves in admin
+    @CrossOrigin("*")
+    @GetMapping("/viewAllLeaves")
+    public List<Map<String,String>> viewAllLeaves(){
+        return (List<Map<String, String>>) leaveDao.viewAllLeave();
+    }
+
+
+    //view all leaves pf empl0yee by employee
+    @CrossOrigin("*")
+    @PostMapping("/viewLeaveById")
+    public List<Map<String,String>> viewLeaveById(@RequestBody LeaveModel model){
+        return (List<Map<String, String>>) leaveDao.viewLeaveByEmpID(model.getEmp_id());
+    }
+
 }
--- src/main/java/com/nestdigital/office/dao/LeaveDao.java
+++ src/main/java/com/nestdigital/office/dao/LeaveDao.java
@@ -1,4 +1,19 @@
 package com.nestdigital.office.dao;
+import com.nestdigital.office.model.LeaveModel;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import java.util.List;
+import java.util.Map;
 
-public interface LeaveDao {
+public interface LeaveDao extends CrudRepository<LeaveModel,Integer> {
+    @Modifying
+    @Query(value = "UPDATE `leaves` SET `status`= :status WHERE id = :id",nativeQuery = true)
+    void changeStatusOfLeave(int id,int status);
+    @Query(value = "SELECT l.`id`, l.`status`, l.`apply_date`, l.`emp_id`, l.`leave_date`, l.`type`," +
+            " l.`description`,e.name FROM `leaves` l JOIN employees e ON l.emp_id = e.id",nativeQuery = true)
+    List<Map<String,String>> viewAllLeave();
+    @Query(value = "SELECT l.`id`, l.`status`, l.`apply_date`, l.`emp_id`, l.`leave_date`, l.`type`," +
+            " l.`description`,e.name FROM `leaves` l JOIN employees e ON l.emp_id = e.id WHERE l.emp_id = :id",nativeQuery = true)
+    List<Map<String,String>> viewLeaveByEmpID(int id);
 }
